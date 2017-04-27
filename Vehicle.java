c7ab2341b2a991187a91224f8ef6e4e33b8e7f38
marcospgp <marcospereiradev@gmail.com>
2017-04-27 17:21:05 +0100

    inicialização de variaveis de instancia
    inicialização de codigo

---
+++ Vehicle.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class Vehicle here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Vehicle {
+    
+   // Variaveis de instancia //
+   private int velocidadePkm;
+   private int preçoPkm;
+   private int fatorfiabilidade;
+   private boolean waitlist;
+}
---
+++ Vehicletype.java
@@ -0,0 +1,11 @@
+
+/**
+ * Write a description of class Vehicletype here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Vehicletype extends Vehicle {
+   private int lugares;
+   
+}
