fe2107944c2e0b2219c5e8a5f545e4dae1fa16a3
marcospgp <marcospereiradev@gmail.com>
2017-04-27 18:03:45 +0100

    adições a class veiculo
    mudanças em veiculos

--- Vehicle.java
+++ Vehicle.java
@@ -8,8 +8,42 @@
 public class Vehicle {
     
    // Variaveis de instancia //
-   private int velocidadePkm;
-   private int preçoPkm;
-   private int fatorfiabilidade;
-   private boolean waitlist;
+   private int speedPkm;
+   private int pricePkm;
+   private int reliabilityFactor;
+   private boolean waitlistStatus;
+   
+   
+   /**getters setters **/
+   public int getSpeed() {
+       return this.speedPkm;
+   }
+   
+   public void setSpeed(int speed) {
+       this.speedPkm = speed;
+   }
+   
+   public int getPrice() {
+       return this.pricePkm;
+   }
+   
+   public void setPrice(int price) {
+       this.pricePkm = price;
+   }
+   
+   public int getFactor() {
+       return this.reliabilityFactor;
+   }
+   /** enquanto não randomizamos, factor set to 100**/
+   public void setFactor(int factor) {
+       this.reliabilityFactor = 100;
+   }
+   
+   public boolean getWaitlistStatus() {
+       return this.waitlistStatus;
+   }
+   
+   public void setWaitlistStatus(boolean status) {
+       this.waitlistStatus = status;
+   }
 }
--- Vehicletype.java
+++ Vehicletype.java
@@ -6,6 +6,15 @@
  * @version (a version number or a date)
  */
 public class Vehicletype extends Vehicle {
+   /** variaveis de instancia **/
    private int lugares;
    
-}
+   /** getters e setters **/
+   public int getLugares() {
+       return this.lugares;
+   }
+   
+   public void setLugares(int numLugares) {
+       this.lugares = numLugares;
+   }
+}   
