df92258108a326d0af6bb3073ddfebfc7f8d8a36
bfar97 <bfar97@gmail.com>
2017-04-28 12:19:08 +0100

    variaveis de viagens

--- Viagens.java
+++ Viagens.java
@@ -5,29 +5,13 @@
  * @author (seu nome) 
  * @version (número de versão ou data)
  */
-public class Viagens
-{
-    // variáveis de instância - substitua o exemplo abaixo pelo seu próprio
-    private int x;
+public class Viagens {
+    
+    // variaveis de instancia
+    private String PdePartida;
+    private Ponto2D Cpartida;
+    private String PdeChegada;
+    private Ponto2D Cchegada;
+    
 
-    /**
-     * COnstrutor para objetos da classe Viagens
-     */
-    public Viagens()
-    {
-        // inicializa variáveis de instância
-        x = 0;
-    }
-
-    /**
-     * Exemplo de método - substitua este comentário pelo seu próprio
-     * 
-     * @param  y   exemplo de um parâmetro de método
-     * @return     a soma de x com y 
-     */
-    public int sampleMethod(int y)
-    {
-        // ponha seu código aqui
-        return x + y;
-    }
 }
