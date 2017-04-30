e9f709b4ba6a2038db3df3f9d241624a67b41ea5
bernardo <bernardo@bernardo-pc.lan>
2017-04-30 17:21:34 +0100

    added getters setters to vehicle class

--- Vehicle.java
+++ Vehicle.java
@@ -14,6 +14,13 @@ public class Vehicle {
    private Ponto2D position;
    private Driver driver;
    
+   public Vehicle() {
+       matricula = "N/A";
+       type = VehicleType.LIGEIROS;
+       position = new Ponto2D();
+       driver = new Driver();
+   }
+   
    public Vehicle(String matr, VehicleType type, double posX, double posY, Driver driver) {
        this.matricula = matr;
        this.type = type;
@@ -26,5 +33,39 @@ public class Vehicle {
        type = car.type;
        position = car.position;
        driver = car.driver;
+   }
+   
+   /** Getters **/
+   public String getMatricula() {
+       return this.matricula;
+   }
+   
+   public VehicleType getType() {
+       return this.type;
+   }
+   
+   public Ponto2D getPosition() {
+       return this.position;
+   }
+   
+   public Driver getDriver() {
+       return this.driver;
+   }
+   
+   /** Setters **/
+   public void setMatricula(String matr) {
+       this.matricula = matr;
+   }
+   
+   public void setType(VehicleType type) {
+       this.type = type;
+   }
+   
+   public void setPosition(double posX, double posY) {
+       this.position = new Ponto2D(posX, posY);
+   }
+   
+   public void setDriver(Driver driver) {
+       this.driver = driver;
    }
 }
